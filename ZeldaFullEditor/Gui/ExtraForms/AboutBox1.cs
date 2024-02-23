5e3f9ed3cda656312edd477a8d56cc51d57d85ac
spannerisms <32842036+spannerisms@users.noreply.github.com>
2024-02-23 02:32:51 -0500

    better stuff
    will format more later

--- ZeldaFullEditor/Gui/ExtraForms/AboutBox1.cs
+++ ZeldaFullEditor/Gui/ExtraForms/AboutBox1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace ZeldaFullEditor
@@ -6,12 +7,75 @@ namespace ZeldaFullEditor
 	partial class AboutBox1 : Form
 	{
 		// TODO KAN REFACTOR - remove all the hardcoded label text and use an array of strings in this file
+		// "
+
+		private static readonly Font HeaderFont = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Bold, GraphicsUnit.Point);
+		private static readonly Font CreditFont = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+
 		public AboutBox1()
 		{
 			InitializeComponent();
-			this.Text = $"About {UIText.APPNAME}";
-			this.AboutVersion.Text = string.Format("Version: {0}", UIText.VERSION);
+			Text = $"About {UIText.APPNAME}";
+
+			CreditsPanel.Controls.Clear();
+
+			foreach (var s in Credits)
+			{
+				var text = s;
+				bool header = false;
+
+				if (s[0] is '#')
+				{
+					text = s.Substring(1);
+					header = true;
+				}
+
+				var add = new Label()
+				{
+					Text = text,
+					BackColor = Color.Transparent,
+					ForeColor = Color.Black,
+					Width = 500,
+					Height = 17,
+					Padding = new Padding(0, 0, 0, 0),
+					Margin = new Padding(0, 0, 0, 0),
+					MinimumSize = new Size(500, 17),
+					Font = header ? HeaderFont : CreditFont,
+					Dock = DockStyle.Top
+				};
+
+				CreditsPanel.Controls.Add(add);
+			}
+
+			CreditsPanel.PerformLayout();
+			CreditsPanel.Invalidate();
+
+			AboutVersion.Text = string.Format("Version: {0}", UIText.VERSION);
 		}
+
+		private static readonly string[] Credits =
+		{
+			// use # for headers
+			"#Main author",
+			"Zarby89",
+
+			"#Other contributors",
+			"kan - Code and logo",
+			"Jared_Brian_ - Code",
+			"Sosuke3 - Code and decompression library",
+			"Scawful - Code",
+
+			"#Beta testers",
+			"Letterbomb",
+			"Jeimuzu",
+
+			"#Special thanks",
+			"MathOnNapkins - Documentation and disassembly",
+			"Sephiroth3 - Hyrule Magic (used as reference)",
+			"SePH - Documentation",
+			"The general ALttP hacking community and Zeldix"
+		};
+
 
 		private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
 		{
