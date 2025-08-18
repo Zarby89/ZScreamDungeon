5bb7072a407d885dd1c28ad86b2e74c6dd1733e2
Jared_Brian_ <jaredbrian5828@gmail.com>
2025-08-18 12:24:02 -0600

    fixed the ow tile selecting box when selecting negative

--- ZeldaFullEditor/Gui/Scene/SceneOW.cs
+++ ZeldaFullEditor/Gui/Scene/SceneOW.cs
@@ -855,15 +855,20 @@ namespace ZeldaFullEditor
 
             g.CompositingMode = CompositingMode.SourceOver;
 
+            int MouseRealXScaled = (this.mouseX_Real / 16);
+            int MouseRealYScaled = (this.mouseY_Real / 16);
+
             if (this.selecting)
             {
-                g.DrawRectangle(Pens.White, new Rectangle(this.globalmouseTileDownX * 16, this.globalmouseTileDownY * 16, (((this.mouseX_Real / 16) - this.globalmouseTileDownX) * 16) + 16, (((this.mouseY_Real / 16) - this.globalmouseTileDownY) * 16) + 16));
+                int leftMost = this.globalmouseTileDownX <= MouseRealXScaled ? this.globalmouseTileDownX : MouseRealXScaled;
+                int topMost = this.globalmouseTileDownY <= MouseRealYScaled ? this.globalmouseTileDownY : MouseRealYScaled;
+                g.DrawRectangle(Pens.White, new Rectangle(leftMost * 16, topMost * 16, Math.Abs(((MouseRealXScaled - this.globalmouseTileDownX) * 16) + 16), Math.Abs(((MouseRealYScaled - this.globalmouseTileDownY) * 16) + 16)));
             }
 
             if (this.selectedMode == ObjectMode.OWDoor || this.selectedMode == ObjectMode.Tile)
             {
-                g.DrawImage(this.tilesgfxBitmap, new Rectangle((this.mouseX_Real / 16) * 16, (this.mouseY_Real / 16) * 16, this.selectedTileSizeX * 16, (this.selectedTile.Length / this.selectedTileSizeX) * 16), 0, 0, this.selectedTileSizeX * 16, (this.selectedTile.Length / this.selectedTileSizeX) * 16, GraphicsUnit.Pixel, ia);
-                g.DrawRectangle(Pens.LightGreen, new Rectangle((this.mouseX_Real / 16) * 16, (this.mouseY_Real / 16) * 16, this.selectedTileSizeX * 16, (this.selectedTile.Length / this.selectedTileSizeX) * 16));
+                g.DrawImage(this.tilesgfxBitmap, new Rectangle(MouseRealXScaled * 16, MouseRealYScaled * 16, this.selectedTileSizeX * 16, (this.selectedTile.Length / this.selectedTileSizeX) * 16), 0, 0, this.selectedTileSizeX * 16, (this.selectedTile.Length / this.selectedTileSizeX) * 16, GraphicsUnit.Pixel, ia);
+                g.DrawRectangle(Pens.LightGreen, new Rectangle(MouseRealXScaled * 16, MouseRealYScaled * 16, this.selectedTileSizeX * 16, (this.selectedTile.Length / this.selectedTileSizeX) * 16));
             }
 
             if (showLinkCamera)
